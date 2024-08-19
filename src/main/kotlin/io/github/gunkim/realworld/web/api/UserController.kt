0f46ebb681f6fe3a623f5ba781ecd22ccb922731
gunkim <gunkim.dev@gmail.com>
2024-08-19 18:40:58 +0900

    Refactor UserResponse creation in UserController

--- src/main/kotlin/io/github/gunkim/realworld/web/api/UserController.kt
+++ src/main/kotlin/io/github/gunkim/realworld/web/api/UserController.kt
@@ -3,6 +3,7 @@ package io.github.gunkim.realworld.web.api
 import io.github.gunkim.realworld.application.JwtProvider
 import io.github.gunkim.realworld.application.UserService
 import io.github.gunkim.realworld.domain.user.Email
+import io.github.gunkim.realworld.domain.user.User
 import io.github.gunkim.realworld.web.model.AuthenticatedUser
 import io.github.gunkim.realworld.web.request.UserAuthenticateRequest
 import io.github.gunkim.realworld.web.request.UserRegistrationRequest
@@ -22,7 +23,7 @@ class UserController(
         request: UserRegistrationRequest,
     ): UserResponse {
         val registeredUser = userService.registerUser(request)
-        return UserResponse.from(registeredUser, jwtProvider.create(registeredUser.id!!))
+        return createUserResponse(registeredUser)
     }
 
     @PostMapping("/login")
@@ -31,10 +32,8 @@ class UserController(
         request: UserAuthenticateRequest,
     ): UserResponse {
         val user = userService.findUserByEmail(Email(request.email))
-
         userService.authenticate(user, request.password)
-
-        return UserResponse.from(user, jwtProvider.create(user.id!!))
+        return createUserResponse(user)
     }
 
     @GetMapping
@@ -43,6 +42,11 @@ class UserController(
         authenticatedUser: AuthenticatedUser,
     ): UserResponse {
         val user = userService.findUserById(authenticatedUser.id)
-        return UserResponse.from(user, jwtProvider.create(user.id!!))
+        return createUserResponse(user)
+    }
+
+    private fun createUserResponse(user: User): UserResponse {
+        val token = jwtProvider.create(user.id!!)
+        return UserResponse.from(user, token)
     }
 }
