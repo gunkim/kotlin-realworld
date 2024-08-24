fbddacf5cc1c2d4da16486cdcb537b49f59f9ef1
gunkim <gunkim.dev@gmail.com>
2024-08-25 00:03:27 +0900

    Add initial database schema and configure schema initialization

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,52 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id         UUID                NOT NULL PRIMARY KEY,
+    email      VARCHAR(255) UNIQUE NOT NULL,
+    password   VARCHAR(255)        NOT NULL,
+    bio        VARCHAR(255),
+    image      VARCHAR(255),
+    name       VARCHAR(255),
+    created_at TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP(6)
+);
+
+CREATE TABLE IF NOT EXISTS article
+(
+    id          UUID NOT NULL PRIMARY KEY,
+    title       VARCHAR(255),
+    description VARCHAR(255),
+    body        VARCHAR(255),
+    created_at  TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP(6)
+);
+
+CREATE TABLE IF NOT EXISTS comment
+(
+    id         UUID NOT NULL PRIMARY KEY,
+    article_id UUID NOT NULL,
+    body       VARCHAR(255),
+    created_at TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP(6),
+    CONSTRAINT fk_comment_article FOREIGN KEY (article_id) REFERENCES article (id)
+);
+
+CREATE TABLE IF NOT EXISTS tag
+(
+    id         UUID NOT NULL PRIMARY KEY,
+    article_id UUID,
+    name       VARCHAR(255),
+    created_at TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP(6),
+    CONSTRAINT fk_tag_article FOREIGN KEY (article_id) REFERENCES article (id)
+);
+
+CREATE TABLE IF NOT EXISTS user_follow
+(
+    id          UUID NOT NULL PRIMARY KEY,
+    followee_id UUID NOT NULL,
+    follower_id UUID NOT NULL,
+    created_at  TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (follower_id, followee_id),
+    CONSTRAINT fk_user_follow_followee FOREIGN KEY (followee_id) REFERENCES users (id),
+    CONSTRAINT fk_user_follow_follower FOREIGN KEY (follower_id) REFERENCES users (id)
+);
