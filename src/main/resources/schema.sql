b17ce9b0dbde3f1df269d6e594b5c0a670ad9f91
gunkim <gunkim.dev@gmail.com>
2024-08-25 15:40:39 +0900

    Add unique constraint to 'name' column in users table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS users
 (
     id         UUID                NOT NULL PRIMARY KEY,
     email      VARCHAR(255) UNIQUE NOT NULL,
+    name       VARCHAR(255) UNIQUE NOT NULL,
     password   VARCHAR(255)        NOT NULL,
     bio        VARCHAR(255),
     image      VARCHAR(255),
-    name       VARCHAR(255),
     created_at TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP(6)
 );
