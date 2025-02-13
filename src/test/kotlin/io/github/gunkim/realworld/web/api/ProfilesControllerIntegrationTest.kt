203b03a1ddd0d166482ae83b3181c5ea01591f23
gunkim <gunkim.dev@gmail.com>
2025-02-13 13:27:18 +0900

    Add follow and unfollow profile integration tests

--- src/test/kotlin/io/github/gunkim/realworld/web/api/ProfilesControllerIntegrationTest.kt
+++ src/test/kotlin/io/github/gunkim/realworld/web/api/ProfilesControllerIntegrationTest.kt
@@ -3,15 +3,19 @@ package io.github.gunkim.realworld.web.api
 import io.github.gunkim.realworld.application.AuthenticationService
 import io.github.gunkim.realworld.domain.user.service.AuthenticateUserService
 import io.github.gunkim.realworld.domain.user.service.CreateUserService
+import io.github.gunkim.realworld.domain.user.service.FollowUserService
 import io.kotest.core.annotation.Tags
 import io.kotest.core.spec.DisplayName
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.extensions.spring.SpringExtension
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpHeaders
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.delete
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.post
 import org.springframework.transaction.annotation.Transactional
 
 private const val AUTHORIZATION_HEADER = HttpHeaders.AUTHORIZATION
@@ -27,7 +31,10 @@ class ProfilesControllerIntegrationTest(
     @Autowired private val createUserService: CreateUserService,
     @Autowired private val authenticationService: AuthenticationService,
     @Autowired private val authenticateUserService: AuthenticateUserService,
+    @Autowired private val followUserService: FollowUserService
 ) : StringSpec({
+    extension(SpringExtension)
+
     val rawPassword = "password"
     val authenticatedUser = createUserService.createUser(
         email = "authuser@example.com",
@@ -40,7 +47,6 @@ class ProfilesControllerIntegrationTest(
         encodedPassword = authenticateUserService.encodePassword(rawPassword)
     )
     val token = authenticationService.createToken(authenticatedUser.uuid)
-    val authorizationToken = "$BEARER_TOKEN_PREFIX$token"
 
     "GET /api/profiles/:username - Return profile without authentication" {
         mockMvc.get("/api/profiles/${targetUser.name}")
@@ -53,15 +59,21 @@ class ProfilesControllerIntegrationTest(
             }
     }
 
-    "GET /api/profiles/:username - Return profile with authentication (not following)" {
-        mockMvc.get("/api/profiles/${targetUser.name}") {
-            header(AUTHORIZATION_HEADER, authorizationToken)
+    "POST /api/profiles/:username/follow - Follow a profile" {
+        mockMvc.post("/api/profiles/${targetUser.name}/follow") {
+            header(AUTHORIZATION_HEADER, "$BEARER_TOKEN_PREFIX$token")
         }.andExpect {
             status { isOk() }
-            jsonPath("$.profile.username") { value(targetUser.name) }
-            jsonPath("$.profile.bio") { value(targetUser.bio) }
-            jsonPath("$.profile.image") { value(targetUser.image) }
-            jsonPath("$.profile.following") { value(false) }
+        }
+    }
+
+    "DELETE /api/profiles/:username/follow - Unfollow a profile" {
+        followUserService.followUser(authenticatedUser.uuid, targetUser.name)
+
+        mockMvc.delete("/api/profiles/${targetUser.name}/follow") {
+            header(AUTHORIZATION_HEADER, "$BEARER_TOKEN_PREFIX$token")
+        }.andExpect {
+            status { isOk() }
         }
     }
 })
